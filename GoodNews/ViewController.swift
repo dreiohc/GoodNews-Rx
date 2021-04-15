78aae82d7e46568400e6dd875e9908e1c185e146
Myron Dulay <dreiohc@live.com>
2021-04-15 22:09:18 +0800

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Myron Dulay on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
